ab956bfefd6b70cf5851ff7ec6397b93332b0919
Cecile Costomiris <cecile.costomiris@gmail.com>
2018-06-23 19:41:15 +0200

    Ajout de exo_3.rb

--- exo_03.rb
+++ exo_03.rb
@@ -1,4 +1,3 @@
-puts "Bonjour monde !"
-#printf "Et avec une voix sexy, ça donne : Bonjour, monde !"
-
 # Tout se qui se trouve après le # devient gris. Le signe # dans Ruby est destiné aux commentaires.
+puts "Bonjour monde !"
+# printf "Et avec une voix sexy, ça donne : Bonjour, monde !"
