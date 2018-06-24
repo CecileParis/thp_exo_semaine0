741a45a668b2766baaaba1218b6f26a523afca14
Cecile Costomiris <cecile.costomiris@gmail.com>
2018-06-24 11:54:29 +0200

    Ajout de exo_07_b.rb

---
+++ exo_07_b.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print ">cecile "
+# Il est demandé à l'utilisateur d'indiquer son prénom qui sera ensuite inséré par Ruby
+user_name = gets.chomp
+# Le prénom de l'utilisateur est inséré par Ruby
+puts user_name
