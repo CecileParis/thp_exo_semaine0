4259e4be3379ca64e79f5a796e4bfbf9406bb55b
Cecile Costomiris <cecile.costomiris@gmail.com>
2018-06-24 12:01:21 +0200

    Ajout de exo_07_c.rb

---
+++ exo_07_c.rb
@@ -0,0 +1,5 @@
+# La différence entre ces trois programmes est la façon dont est demandée l'information qui influence du coup la façon dont Ruby va lire le programme.
+# il est demandé à l'utilisateur d'indiquer son prénom.
+user_name = gets.chomp
+# Ruby affiche le prénom de l'utilisateur
+puts user_name
